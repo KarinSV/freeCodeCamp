57f3f205e9acf310871b5aa549d1b18e0f9db4ed
KarinSV <karina.svanstrom@gmail.com>
2023-01-11 10:27:55 +0100

    Selecting from Many Options with Switch Statements

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic JavaScript/76.js
@@ -0,0 +1,31 @@
+// Selecting from Many Options with Switch Statements
+
+function caseInSwitch(val) {
+    let answer = "";
+    switch (val){
+      case 1:
+        answer = "alpha";
+        break;
+       case 2:
+        answer = "beta";
+        break;
+       case 3:
+        answer = "gamma";
+        break;
+       case 4:
+        answer = "delta";
+        break;
+    }
+  
+    return answer;
+  }
+  
+  console.log(caseInSwitch(3));
+
+//   Write a switch statement which tests val and sets answer for the following conditions:
+// 1 - alpha
+// 2 - beta
+// 3 - gamma
+// 4 - delta
+
+// case values are tested with strict equality (===). The break tells JavaScript to stop executing statements. If the break is omitted, the next statement will be executed.
