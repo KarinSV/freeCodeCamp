d10c6c99f5fc70828e7236e7fb29b59efaf99cb1
KarinSV <karina.svanstrom@gmail.com>
2023-01-11 14:18:25 +0100

    switch statement

--- JavaScript_Algorithms_and_Data_Structures/Basic JavaScript/76.js
+++ JavaScript_Algorithms_and_Data_Structures/Basic JavaScript/76.js
@@ -1,5 +1,13 @@
 // Selecting from Many Options with Switch Statements
 
+// case values are tested with strict equality (===). The break tells JavaScript to stop executing statements. If the break is omitted, the next statement will be executed.
+
+//   Write a switch statement which tests val and sets answer for the following conditions:
+// 1 - alpha
+// 2 - beta
+// 3 - gamma
+// 4 - delta
+
 function caseInSwitch(val) {
     let answer = "";
     switch (val){
@@ -21,11 +29,3 @@ function caseInSwitch(val) {
   }
   
   console.log(caseInSwitch(3));
-
-//   Write a switch statement which tests val and sets answer for the following conditions:
-// 1 - alpha
-// 2 - beta
-// 3 - gamma
-// 4 - delta
-
-// case values are tested with strict equality (===). The break tells JavaScript to stop executing statements. If the break is omitted, the next statement will be executed.
