842af5331dce023968416c5e78ccf074b3adeba7
KarinSV <karina.svanstrom@gmail.com>
2023-01-12 17:03:16 +0100

    Build JavaScript Objects

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic JavaScript/83.js
@@ -0,0 +1,10 @@
+// Build JavaScript Objects
+
+const myDog = {
+    // Only change code below this line
+  name: "Sam",
+  legs: 4,
+  tails: 1,
+  friends: ["Tom", "Barry"]
+    // Only change code above this line
+  };
