bbdb1b95cbf14bf44c4e9e0c0de6c4524d5f02e0
KarinSV <karina.svanstrom@gmail.com>
2023-01-12 17:16:50 +0100

    Accessing Object Properties with Variables

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic JavaScript/86.js
@@ -0,0 +1,10 @@
+// Accessing Object Properties with Variables
+
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  const playerNumber = 16;
+  const player = testObj[playerNumber]; 
