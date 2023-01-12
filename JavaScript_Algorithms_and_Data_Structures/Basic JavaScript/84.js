5066b821067bf08220e9e2d2cede42bdfd27f8b6
KarinSV <karina.svanstrom@gmail.com>
2023-01-12 17:07:02 +0100

    Accessing Object Properties with Dot Notation

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic JavaScript/84.js
@@ -0,0 +1,10 @@
+// Accessing Object Properties with Dot Notation
+
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  const hatValue = testObj.hat;     
+  const shirtValue = testObj.shirt; 
