aa973a7c8463fb4c768b93a4ed97609772143c48
KarinSV <karina.svanstrom@gmail.com>
2023-01-13 17:09:27 +0100

    Manipulating Complex Objects

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic JavaScript/92.js
@@ -0,0 +1,24 @@
+// Manipulating Complex Objects
+
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+    },
+    {
+      "artist": "Taylor Swift",
+      "title": "Midnights",
+      "release_year": 2022,
+      "formats": [
+        "mp3",
+        "CD",
+        "LP"
+      ],
+    }
+  ];
